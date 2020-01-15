27a23ecef0293d7543d04a96af510c2302bf426d
Wytze Jan Riedstra <wjriedstra@gmail.com>
2020-01-15 07:09:46 +0100

    pending Import, Export and Module Data (130)

--- library_app/tests/test_book.py
+++ library_app/tests/test_book.py
@@ -1,5 +1,6 @@
 from odoo.tests.common import TransactionCase
 
+
 class TestBook(TransactionCase):
 
     def setUp(self, *args, **kwargs):
--- library_member/tests/test_book.py
+++ library_member/tests/test_book.py
@@ -8,16 +8,16 @@ class TestBook(TransactionCase):
         user_admin = self.env.ref('base.user_admin')
         self.env = self.env(user=user_admin)
         self.Book = self.env['library.book']
-        self.book_lotf = self.Book.create({
+        self.book_two = self.Book.create({
             'name': 'Lord of the Flies',
-            'isbn': '0-571-05686-51234512345'
+            'isbn': '0-571-05686-5'
         })
         return result
 
     def test_create(self):
         """Test Books are active by default"""
-        self.assertEqual(self.book_lotf.active, True)
+        self.assertEqual(self.book_two.active, True)
 
     def test_check_isbn(self):
         """Check valid 10-digit ISBN"""
-        self.assertTrue(self.book_lotf._check_isbn)
+        self.assertTrue(self.book_two._check_isbn)
